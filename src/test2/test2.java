ac14f04aa6308cfe677e4152e58858475f3534ad
ljk23 <tp5admin@126.com>
2017-10-26 11:22:12 +0800

    第一次修改

--- src/test2/test2.java
+++ src/test2/test2.java
@@ -2,6 +2,7 @@ package test2;
 
 public class test2 {
 	public static void main(String[] args){
-        System.out.println("Hello github!");
+        System.out.println("Hello china!");
+        System.out.println("第一次修改!");
     }
 }
